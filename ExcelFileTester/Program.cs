cf551f480437437760a6559c22334ec142384320
tghosth <janda26@hotmail.com>
2016-01-31 14:22:50 +0200

    Add test

--- ExcelFileTester/Program.cs
+++ ExcelFileTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,22 @@ namespace ExcelFileTester
 {
     class Program
     {
+
+        public static string FilePath = @"Z:\host\Users\joshg\Documents\Clients\Ministry of Health\IAM Project\ExcelTest";
+
         static void Main(string[] args)
         {
+
+            // This doesnt actually work, it just goes on file extension!?!?!?!?!
+
+            foreach (string path in Directory.GetFiles(FilePath))
+            {
+                Console.WriteLine(string.Format("\"{0}\":", path));
+                Console.WriteLine("File type is: {0}", MimeTypes.GetContentType(path));
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
